424b6608fe877a7f3197444a28466555f27d6ceb
alexfont <alexfont321@gmail.com>
2018-08-03 15:04:21 -0500

    first commit

--- main.js
+++ main.js
@@ -7,12 +7,19 @@ button.addEventListener("click", function(){
 });
 
 
+//////////////
+
 //print the junior instructors to the DOM
-let jrInstructors = document.getElementById("jr-instructors")
-staff.forEach(jrInstructor => {
-    jrInstructors.appendChild(`${jrInstructor} is a junior instructor at NSS`);
+let jrInstructors = document.getElementById("jr-instructors");
+
+
+staff.juniorInstructors.forEach(jrInstructor => {
+    let createP = document.createElement('p');
+    createP.innerText = `${jrInstructor} is a junior instructor at NSS.`;
+    jrInstructors.appendChild(createP); 
 });
 
+///////////
 
 //console log each of the schools in the school array individually and console log them
 schools.forEach(schools => {
@@ -20,16 +27,24 @@ schools.forEach(schools => {
 });
 
 
+////////////
+
 //print each of the lead instructors to the console
-for(let i = 0; i <= staff.instructors.length; i++){
+for (let i = 0; i < staff.instructors.length; i++){
     console.log(staff.instructors[i]);
 }
+
+////////////
 
 
 //print the result to the console
 let parkInfo = {};
+
+
 fetch("https://external-data-56670.firebaseio.com/park-info.json")
 .then(result => {
-    parkInfo = result;
-});
-console.log(parkInfo);
+
+    parkInfo = result.json();
+    return parkInfo
+})
+.then(result => console.log(result));
